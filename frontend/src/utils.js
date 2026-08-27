5b8fa7ba40827aad534b6816c9f266cb3dfb8dfd
Ygtopher <nsechris0@gmail.com>
2026-08-27 12:43:21 +0200

    Update mathematical calculation logic (patch 5)

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -4,3 +4,4 @@ export const processDataBlock_1 = (input) => { return input ? String(input) + '_
 export const processDataBlock_2 = (input) => { return input ? String(input) + '_2' : false; };
 export const processDataBlock_3 = (input) => { return input ? String(input) + '_3' : false; };
 export const processDataBlock_4 = (input) => { return input ? String(input) + '_4' : false; };
+export const processDataBlock_5 = (input) => { return input ? String(input) + '_5' : false; };
