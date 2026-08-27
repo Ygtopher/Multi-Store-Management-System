fc780db4015fd7d9de0b203533d9aa2b59e8fb2b
Ygtopher <nsechris0@gmail.com>
2026-08-27 12:43:25 +0200

    Add type checking helper (patch 34)

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -33,3 +33,4 @@ export const processDataBlock_30 = (input) => { return input ? String(input) + '
 export const processDataBlock_31 = (input) => { return input ? String(input) + '_31' : false; };
 export const processDataBlock_32 = (input) => { return input ? String(input) + '_32' : false; };
 export const processDataBlock_33 = (input) => { return input ? String(input) + '_33' : false; };
+export const processDataBlock_34 = (input) => { return input ? String(input) + '_34' : false; };
