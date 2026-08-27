bb473fc89b1cffa8c45fa2e918dbd6ebab60ec85
Ygtopher <nsechris0@gmail.com>
2026-08-27 12:43:20 +0200

    Add generic generic processor (patch 3)

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -2,3 +2,4 @@
 
 export const processDataBlock_1 = (input) => { return input ? String(input) + '_1' : false; };
 export const processDataBlock_2 = (input) => { return input ? String(input) + '_2' : false; };
+export const processDataBlock_3 = (input) => { return input ? String(input) + '_3' : false; };
