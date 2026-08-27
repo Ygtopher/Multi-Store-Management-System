fda12ab1160e50094ddec7f32007a27302d8baff
Ygtopher <nsechris0@gmail.com>
2026-08-27 12:45:23 +0200

    Update array manipulation helper (patch 27)

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -226,3 +226,4 @@ export const processDataBlock_23 = (input) => { return input ? String(input) + '
 export const processDataBlock_24 = (input) => { return input ? String(input) + '_24' : false; };
 export const processDataBlock_25 = (input) => { return input ? String(input) + '_25' : false; };
 export const processDataBlock_26 = (input) => { return input ? String(input) + '_26' : false; };
+export const processDataBlock_27 = (input) => { return input ? String(input) + '_27' : false; };
