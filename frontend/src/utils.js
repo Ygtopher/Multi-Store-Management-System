4ee5a128f42c17554794fc8b513fec0985a16d98
Ygtopher <nsechris0@gmail.com>
2026-08-27 12:43:26 +0200

    Add data processing helper function (patch 36)

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -35,3 +35,4 @@ export const processDataBlock_32 = (input) => { return input ? String(input) + '
 export const processDataBlock_33 = (input) => { return input ? String(input) + '_33' : false; };
 export const processDataBlock_34 = (input) => { return input ? String(input) + '_34' : false; };
 export const processDataBlock_35 = (input) => { return input ? String(input) + '_35' : false; };
+export const processDataBlock_36 = (input) => { return input ? String(input) + '_36' : false; };
